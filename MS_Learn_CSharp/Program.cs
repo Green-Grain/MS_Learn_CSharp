4d98c0616bef8239f695062bb5abdb28c6d9302f
Green-Grain <zwei.blattes@gmail.com>
2020-05-07 16:14:59 +0900

    最初の C# コードを記述する 5/7

--- MS_Learn_CSharp/Program.cs
+++ MS_Learn_CSharp/Program.cs
@@ -6,11 +6,10 @@ namespace MS_Learn_CSharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-
-      Console.Write("Congratulations!");
-      Console.Write(" ");
-      Console.Write("You wrote your first lines of code!");
+      Console.WriteLine("This is the first line.");
+      Console.Write("This is ");
+      Console.Write("the first line.");
+      Console.Write("\n");
     }
   }
 }
