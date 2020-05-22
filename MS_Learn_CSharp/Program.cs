4488f549f5f73217e7da81c4657a7f150a8f4cce
Green-Grain <zwei.blattes@gmail.com>
2020-05-22 22:04:41 +0900

    C# で数値に基本的操作を実行する

--- MS_Learn_CSharp/Program.cs
+++ MS_Learn_CSharp/Program.cs
@@ -6,17 +6,8 @@ namespace MS_Learn_CSharp
   {
     static void Main(string[] args)
     {
-      string projectName = "ACME";
-
-      string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-
-      string pathPrefix = @"c:\Exercise\";
-      string pathSuffix = @"\data.txt";
-      string path = $"{pathPrefix}{projectName}{pathSuffix}";
-      Console.WriteLine($"View English output:\n\t\t{path}\n");
-
-      path = $@"{pathPrefix}{projectName}\ru-RU{pathSuffix}";
-      Console.WriteLine($"{russianMessage}\n\t\t{path}\n");
+      int fahrenheit = 94;
+      Console.WriteLine("The temperature is " + (fahrenheit - 32) * ((decimal)5 / 9) + " Celsius.");
     }
   }
 }
